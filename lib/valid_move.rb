eea3e13d3a54e86a0abf0fbba3c4faf3f01b1aac
Kachel Wittig <kjwittig@gmail.com>
2018-02-28 00:40:15 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,7 @@
-# code your #valid_move? method here
-
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def some_new_fabulous_method
+    if position_taken? == between?(board[0], board[8])
+        return board[index]
+    else
+        return false
+    end
+end
