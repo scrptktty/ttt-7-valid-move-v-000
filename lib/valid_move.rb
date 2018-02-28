cce7b3e5e26eb440bb4d13239fb256045f18f56c
Kachel Wittig <kjwittig@gmail.com>
2018-02-28 01:07:30 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,14 +1,14 @@
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] = nil
-    return false
-  else
-    return true
-  end
-end
+# def position_taken?(board, index)
+#   if board[index] == " " || board[index] == "" || board[index] = nil
+#     return false
+#   else
+#     return true
+#   end
+# end
 
 
 def valid_move?(board, index)
-  if index.between?(0,8) == true && position_taken? == false
+  if index.between?(0,8) == true && board[index] == " " || board[index] == "" || board[index] = nil
     return board[index]
   else
     return false
