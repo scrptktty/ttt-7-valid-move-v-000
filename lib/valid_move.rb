6e8012de9c1254dc62e4040ea66a5e255cfa7685
Kachel Wittig <kjwittig@gmail.com>
2018-02-28 01:02:47 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 def valid_move?(board, index)
-    if index.between?(0,8) == true && position_taken? == true
+    if index.between?(0,8) == true && position_taken? == false
         return board[index]
     else
         return false
